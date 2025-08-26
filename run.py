c76e3ba8f1b3b7c71d191188bf34982761ccc4ea
diane-ch <diane.chounlamountry@gmail.com>
2025-08-26 09:59:17 +0100

    railway deploy

--- run.py
+++ run.py
@@ -5,6 +5,6 @@ app = create_app()
 
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    #app.run(host='0.0.0.0', port=5000, debug=True)
     #app.run(host='127.0.0.1', port=5000, debug=True)
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
